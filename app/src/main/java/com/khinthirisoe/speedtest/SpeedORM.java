28c0a74e0188b52cdca15a3685caa53a729225aa
Thiri <khinthirisoe@Khins-MBP>
2017-05-11 20:54:48 +0630

    edit speedORM

--- app/src/main/java/com/khinthirisoe/speedtest/SpeedORM.java
+++ app/src/main/java/com/khinthirisoe/speedtest/SpeedORM.java
@@ -5,9 +5,9 @@ import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 
-import com.khinthirisoe.speedtest.Utils.PrefUtils;
 import com.khinthirisoe.speedtest.database.SpeedDbHelper;
 
+import static com.khinthirisoe.speedtest.database.SpeedContract.SpeedEntry;
 import static com.khinthirisoe.speedtest.database.SpeedContract.SpeedEntry.TABLE_NAME;
 
 /**
@@ -27,18 +27,18 @@ public class SpeedORM {
     public void addSpeed(float download, float upload, float ping, long date) {
         SQLiteDatabase db = dbHelper.getWritableDatabase();
         ContentValues values = new ContentValues();
-        values.put(PrefUtils.PREF_DOWNLOAD, download);
-        values.put(PrefUtils.PREF_UPLOAD, upload);
-        values.put(PrefUtils.PREF_PING, ping);
-        values.put(PrefUtils.PREF_END_DATE, date);
+        values.put(SpeedEntry.COLUMN_DOWNLOAD, download);
+        values.put(SpeedEntry.COLUMN_UPLOAD, upload);
+        values.put(SpeedEntry.COLUMN_PING, ping);
+        values.put(SpeedEntry.COLUMN_DATE, date);
         db.insert(TABLE_NAME, null, values);
         db.close();
     }
 
     public BenchMark findSpeed(long date) {
         SQLiteDatabase db = dbHelper.getReadableDatabase();
-        Cursor cursor = db.query(TABLE_NAME, new String[]{PrefUtils.PREF_DOWNLOAD, PrefUtils.PREF_UPLOAD, PrefUtils.PREF_PING},
-                PrefUtils.PREF_END_DATE + "=?", new String[]{String.valueOf(date)}, null, null, null);
+        Cursor cursor = db.query(TABLE_NAME, new String[]{SpeedEntry.COLUMN_DOWNLOAD, SpeedEntry.COLUMN_UPLOAD, SpeedEntry.COLUMN_PING},
+                SpeedEntry.COLUMN_DATE + "=?", new String[]{String.valueOf(date)}, null, null, null);
         if (cursor != null) {
             cursor.moveToFirst();
         }
